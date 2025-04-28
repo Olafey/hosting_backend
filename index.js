fc4059b8b6963c317e5319f95fb74444608de9eb
Olafey <olamidearemu2000@yahoo.com>
2025-04-28 12:19:25 +0100

    created a route

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Responding to PORT ${PORT}`);
 });
+
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to Hosting API",
+  });
+});
+
+app.get("/api/v1", (req, res) => {
+  res.send("Welcome to Hosting API Version 1");
+});
